1f7cb466b0b1ba714ad430353fd5ac2ab17eb225
Gustav0132 <soaresgustavo622@gmail.com>
2026-05-05 01:09:25 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sb1-8ccwgh6v/', // 🟢 Isso corrige o erro da tela em branco no GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
